edfb4198e782ddaa12aba2e70c5e34a3e8000c54
Maks Leontiev <vk2021god@vk.com>
2023-11-26 17:06:46 +0500

    obnova

--- js/game.js
+++ js/game.js
@@ -23,7 +23,7 @@ score_audio.src = "audio/score.mp3";
 var gap = 90;
 
 // При нажатии на какую-либо кнопку
-document.addEventListener("keydown", moveUp);
+document.addEventListener("click", moveUp);
 
 function moveUp() {
  yPos -= 25;
